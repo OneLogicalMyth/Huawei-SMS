60ded6e59d6b56df729a1cc3e7b0174801636177
Liam Glanfield <liam@glanfield.co.uk>
2018-11-29 22:44:52 +0000

    Update info_example.py

--- info_example.py
+++ info_example.py
@@ -1,18 +1,19 @@
 from HuaweiSMS import HuaweiSMS
+import datetime
 
 # Grab info
 SMS = HuaweiSMS()
 Traffic = SMS.get_traffic()['response']
 Device  = SMS.get_info()['response']
 
-ConnectTime = int(Traffic['CurrentConnectTime']) / 60
-ConnectTime = "%.f" % round(ConnectTime,0)
+ConnectTime = int(Traffic['CurrentConnectTime'])
+ConnectTime = str(datetime.timedelta(seconds=ConnectTime))
 
 print """
 Huawei Device Info:
 	IP Address: {}
 	Signal Strengh: {}/5
-	Minutes Connected: {}
+	Duration: {} (Hours:Minutes:Seconds)
 	DNS Servers: {}
 		     {}
 """.format(Device['WanIPAddress'],Device['SignalIcon'],ConnectTime,Device['PrimaryDns'],Device['SecondaryDns'])
